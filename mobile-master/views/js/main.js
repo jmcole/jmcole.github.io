879e0071385ee41de7267a0b2786a77102c9eace
jmcole <cole_john@att.net>
2016-07-15 18:43:23 -0400

    Changed JS

--- mobile-master/views/js/main.js
+++ mobile-master/views/js/main.js
@@ -448,12 +448,14 @@ var resizePizzas = function(size) {
   }
 
   // Iterates through pizza elements on the page and changes their widths
+  // Moved Var's outside of loop and added pizzaCon instead of calling in each variable
   function changePizzaSizes(size) {
-    var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[0], size);
-    var newwidth = (document.querySelectorAll(".randomPizzaContainer")[0].offsetWidth + dx) + 'px';
-    var length = (document.querySelectorAll(".randomPizzaContainer").length);
+    var pizzaCon = document.getElementsByClassName("randomPizzaContainer");
+    var dx = determineDx(pizzaCon[0], size);
+    var newwidth = (pizzaCon[0].offsetWidth + dx) + 'px';
+    var length = pizzaCon.length;
     for (var i = 0; i < length; i++) {
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+      document.getElementsByClassName("randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
@@ -469,8 +471,10 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
+
+var pizzasDiv = document.getElementById("randomPizzas");//Moved outside FOR loop
+
 for (var i = 2; i < 100; i++) {
-  var pizzasDiv = document.getElementById("randomPizzas");
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
@@ -525,7 +529,7 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
-  for (var i = 0; i < 200; i++) {
+  for (var i = 0; i < 32; i++) { // changed value from 200. 8*4 = 32 pizzas per collumn
     var elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizza.png";
