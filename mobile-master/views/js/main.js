f9ea52a2516c067448c80f31916b129bee15f7b1
jmcole <cole_john@att.net>
2016-07-24 15:34:10 -0400

    update JS

--- mobile-master/views/js/main.js
+++ mobile-master/views/js/main.js
@@ -506,9 +506,12 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.getElementsByClassName('mover');
+  var scrollTop = document.body.scrollTop
+
+  var items = document.getElementsByClassName('mover');// Changed to more effecient getElementsByClassName
+  var scrollTop = document.body.scrollTop;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin((scrollTop / 1250) + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
