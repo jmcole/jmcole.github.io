275738ad7e925be198a4adb2527f125b5696db37
jmcole <cole_john@att.net>
2016-07-26 19:08:43 -0400

    update

--- mobile-master/views-org/js/main.js
+++ mobile-master/views-org/js/main.js
@@ -513,7 +513,8 @@ function updatePositions() {
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin((document.body.scrollTop) + (i % 5));
     //items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
-    items[i].style.transform = 'translateX(' + items[i].basicLeft + 100 * phase + 'px' + ')';
+    var pizzas = items[i].basicLeft + 100 * phase + 'px'
+    items[i].style.transform = 'translateX(' + pizzas + ')';
   }
 
   // User Timing API to the rescue again. Seriously, it's worth learning.
