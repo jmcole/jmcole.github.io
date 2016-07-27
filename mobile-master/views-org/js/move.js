35d00a888927e862bfbb93ad8649e7fe52fec570
jmcole <cole_john@att.net>
2016-07-26 21:10:41 -0400

    Test

---
+++ mobile-master/views-org/js/move.js
@@ -0,0 +1,41 @@
+function updatePositions() {
+  frame++;
+  window.performance.mark("mark_start_frame");
+  var scrollTop = document.body.scrollTop;//Move outside For loop
+  var items = document.getElementsByClassName('mover');// Changed to more effecient getElementsByClassName
+
+  for (var i = 0; i < items.length; i++) {
+    var phase = Math.sin((scrollTop) + (i % 5));
+    var pizzas = items[i].basicLeft + 100 * phase + 'px';// created seperate variable due to high px values
+    items[i].style.transform = 'translateX(' + pizzas + ')';//Changed to transform
+  }
+
+  // User Timing API to the rescue again. Seriously, it's worth learning.
+  // Super easy to create custom metrics.
+  window.performance.mark("mark_end_frame");
+  window.performance.measure("measure_frame_duration", "mark_start_frame", "mark_end_frame");
+  if (frame % 10 === 0) {
+    var timesToUpdatePosition = window.performance.getEntriesByName("measure_frame_duration");
+    logAverageFrame(timesToUpdatePosition);
+  }
+}
+
+// runs updatePositions on scroll
+window.addEventListener('scroll', updatePositions);
+
+// Generates the sliding pizzas when the page loads.
+document.addEventListener('DOMContentLoaded', function() {
+  var cols = 8;
+  var s = 256;
+  for (var i = 0; i < 36; i++) {//changed value from 200 do not need that many pizzas
+    var elem = document.createElement('img');
+    elem.className = 'mover';
+    elem.src = "images/pizza.png";
+    elem.style.height = "100px";
+    elem.style.width = "73.333px";
+    elem.basicLeft = (i % cols) * s;
+    elem.style.top = (Math.floor(i / cols) * s) + 'px';
+    document.querySelector("#movingPizzas1").appendChild(elem);
+  }
+  updatePositions();
+});
