e5669a6cffbd240aaf2b92e635b13af762c253c3
jmcole <cole_john@att.net>
2016-08-01 23:24:16 -0400

    Update

--- mobile-master/views/dst/Gruntfile.js
+++ mobile-master/views/dst/Gruntfile.js
@@ -1,6 +1,79 @@
 module.exports = function(grunt) {
+grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+        critical: {
+            test: {
+                options: {
+                    base: './',
+                    css: [
+                        'css/style.min.css',
+                        'css/bootstrap-grid.min.css'
+                    ],
+                    width: 1280,
+                    height: 768
+                },
+                src: 'pizza.html',
+                dest: 'pizza3.html'
+            }
+        }
 
 
+});
+
+grunt.loadNpmTasks('grunt-critical');
+
+grunt.registerTask('default', ['critical']);
+
+
+}
+
+/*
+    grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    cssmin: {
+  target: {
+    files: [{
+      expand: true,
+      cwd: 'css/',
+      src: ['*.css', '!*.min.css'],
+      dest: 'css/',
+      ext: '.min.css'
+    }]
+  }
+}
+
+});
+
+grunt.loadNpmTasks('grunt-contrib-cssmin');
+
+grunt.registerTask('default', ['cssmin']);
+
+}
+*/
+
+/*
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+        build: {
+            src: 'js/main.js',
+            dest: 'js/main.min.js',
+        }
+    }
+
+});
+
+grunt.loadNpmTasks('grunt-contrib-uglify');
+
+grunt.registerTask('default', ['uglify']);
+
+}
+/*
+
+/*
 grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
 
@@ -24,44 +97,16 @@ grunt.registerTask('default', ['htmlmin']);
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-    pkg: grunt.file.readJSON('package.json'),
-
-    critical: {
-        test: {
-            options: {
-                base: './',
-                css: [
-                    'css/style.min.css',
-                    'css/print.min.css'
-                ],
-                width: 1280,
-                height: 768
-            },
-            src: 'index.html',
-            dest: 'index2.html'
-        }
-    }
-
-
-});
-
-grunt.loadNpmTasks('grunt-critical');
-
-grunt.registerTask('default', ['critical']);
-
-
-}
 */
+
+
+
+
+
+
+
+
+
+
+
+
